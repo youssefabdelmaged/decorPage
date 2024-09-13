ff43fe5b8316d25ad6876b65620ca076d7b9266f
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-09-13 11:30:24 +0300

    +1

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,8 @@
 import image1 from "./Image1.png";
 import image2 from "./Image2.png";
 import image from "./Image.png";
-import logo from "./logo.png";
 import mask1 from "./Maskgroup1.png";
 import mask2 from "./Maskgroup.png";
 import maskface from "./Maskgroup2.png";
-
+import logo from "./logo.png";
 export { image1, image2, image, logo, mask1, mask2, maskface };
