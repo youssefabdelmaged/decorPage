434240a93baa06adc2cbebe5f791af1ba05e8c3b
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-09-13 11:38:22 +0300

    logodepllooy

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,5 +4,5 @@ import image from "./Image.png";
 import mask1 from "./Maskgroup1.png";
 import mask2 from "./Maskgroup.png";
 import maskface from "./Maskgroup2.png";
-import logo from "./logo.png";
+import logo from "../images/Logo.png";
 export { image1, image2, image, logo, mask1, mask2, maskface };
