b974c4c703bbee8c8f3a979ff5944776042edc75
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-09-13 11:26:54 +0300

    namesuodate

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,9 @@
-import image1 from "./Image 1.png";
-import image2 from "./Image 2.png";
+import image1 from "./Image1.png";
+import image2 from "./Image2.png";
 import image from "./Image.png";
 import logo from "./logo.png";
-import mask1 from "./Mask group (1).png";
-import mask2 from "./Mask group.png";
-import maskface from "./Mask group (2).png";
+import mask1 from "./Maskgroup1.png";
+import mask2 from "./Maskgroup.png";
+import maskface from "./Maskgroup2.png";
 
 export { image1, image2, image, logo, mask1, mask2, maskface };
